df667faa9665243010933e9e365a192fbec7120d
RulleR <61748957+Sonlein@users.noreply.github.com>
2021-05-01 01:15:16 +0300

    Update AxisProperties.java

--- 3dGraphs/src/AxisProperties.java
+++ 3dGraphs/src/AxisProperties.java
@@ -15,14 +15,6 @@ public class AxisProperties {
         this.step = (double) Math.round(step * 100) / 100;
     }
 
-    public AxisProperties relativeModifyScale(double scale) {
-        scale = (double) Math.round(scale * 100) / 100;
-        min *= scale;
-        max *= scale;
-        step *= scale;
-        return this;
-    }
-
     public double getMin() {
         return min;
     }
